c863b219af86553381d1b4d9cd55d65c6138cfba
x191051 <eaoliver@br.ibm.com>
2017-11-17 18:49:26 -0200

    Added save method for assignments.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let express = require('express')
-// let bodyParser = require('body-parser')
+let bodyParser = require('body-parser')
 let mongoose = require('mongoose')
 
 let app = express()
@@ -7,25 +7,23 @@ let app = express()
 mongoose.connect('mongodb://user:1234@ds151963.mlab.com:51963/mongohost', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   next()
 })
 
-app.get('/', (req, res) => {
-  console.log('Howdy')
-  res.send({key: 'assignment', value: 'Inteligência Artificial'})
-  // let Assignment = mongoose.model('Assignment', {type: String, subject: String})
-  // let assignment = new Assignment({type: 'Prova', subject: 'Inteligência Artificial'})
-  // assignment.save((err, data) => {
-  //   if (err) {
-  //     res.send(err)
-  //   } else {
-  //     res.send(data)
-  //   }
-  // })
+app.post('/api/save', (req, res) => {
+  let Assignment = mongoose.model('Assignment', {type: String, subject: String})
+  let assignment = new Assignment({type: 'Prova', subject: 'Inteligência Artificial'})
+  assignment.save((err, data) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(data)
+    }
+  })
 })
 
 app.get('/api/assignments', (req, res) => {
