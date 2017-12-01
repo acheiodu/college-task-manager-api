151416ff9139c3d8d36ad67cdf4a0de2c4f75460
x191051 <eaoliver@br.ibm.com>
2017-12-01 11:48:40 -0200

    Checking connection for errors.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let mongoose = require('mongoose')
 
 let assignmentController = require('./controllers/assignment.server.controller')
 
-mongoose.connect('mongodb://user:1234@ds151963.mlab.com:51963/mongohost', {useMongoClient: true});
+mongoose.connect('mongodb://user:1234@ds151963.mlab.com:51963/mongohost', {useMongoClient: true}, (err) => console.log(err));
 mongoose.Promise = global.Promise;
 
 let app = express()
