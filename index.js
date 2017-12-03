fe622c06db83eed0f5fa9b4d63423b2e7b493752
Eduardo Atanazio <acheiodu@gmail.com>
2017-12-03 14:45:16 -0200

    teste

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-let bodyParser = require('body-parser')
 let express = require('express')
 
 let app = express()
 
-app.use(bodyParser.json())
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
 app.route('/')
   .get((req, res) => res.send('Hello, World!'))
 
-let port = env.port || 3000
-
-app.listen(port, () => console.log(`Serving on port ${port}...`))
+app.listen(8080, () => console.log(`Server running on port 8080...`))
